078bb60ddcb0bef9ff8845bd28b27cfe37a39614
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-02-21 18:23:11 -0600

    added bear from to DOM

---
+++ src/javascripts/components/initialScreen.js
@@ -0,0 +1,9 @@
+const initialScreen = () => {
+  document.querySelector('body').innerHTML = `
+  <div id="main-container">
+  <h1 class="mb-4">Bear Watcher</h1>
+    <div id="form-container"></div>
+  </div>`;
+};
+
+export default initialScreen;
